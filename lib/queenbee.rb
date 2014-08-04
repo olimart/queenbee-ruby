73a57ac6ed83403ccdb2e11c5f7ae20fe1398e8e
Olivier <olivier.simart@gmail.com>
2014-08-04 15:42:31 -0400

    Syntax and indentation

--- lib/queenbee.rb
+++ lib/queenbee.rb
@@ -23,104 +23,104 @@ require "queenbee/errors/invalid_request_error"
 require "queenbee/errors/authentication_error"
 
 module Queenbee
-    @api_base = "http://localhost:3000/api"
+  @api_base = "http://localhost:3000/api"
 
-    class << self
-      attr_accessor :token, :api_base, :verify_ssl_certs, :api_version
+  class << self
+    attr_accessor :token, :api_base, :verify_ssl_certs, :api_version
+  end
+
+  # self.token = config.queenbee_token if config
+
+  def self.api_url(url="")
+    @api_base + url
+  end
+
+  def self.request(method, url, token, params={}, headers={})
+    unless token ||= @token
+      raise AuthenticationError.new("No token provided")
     end
 
-    # self.token = config.queenbee_token if config
+    url = api_url(url)
 
-    def self.api_url(url="")
-      @api_base + url
+    begin
+      # response = execute_request(request_opts)
+      uri = URI(url)
+      request = Net::HTTP::Post.new(uri)
+
+      request["User-Agent"] = "Queenbee gem"
+      request["Authorization"] = "Token token=\"#{token}\""
+      request["Content-Type"] = "application/json"
+
+      request.body = params.to_json
+
+      response = Net::HTTP.start(uri.hostname, uri.port) {|http|
+        http.request(request)
+      }
+
+      # since http.request doesn't throw any exceptions
+      handle_api_error(response.code, response.body)
+
+      puts response.code
+    rescue SocketError => e
+        handle_connection_error(e)
+    rescue NoMethodError => e
+        handle_connection_error(e)
+    rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,
+        Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError => e
+        handle_connection_error(e)
     end
 
-    def self.request(method, url, token, params={}, headers={})
-      unless token ||= @token
-        raise AuthenticationError.new("No token provided")
-      end
+    [response, token]
+  end
 
-      url = api_url(url)
+  def self.handle_connection_error(e)
+    case e
+      when SocketError
+        message = "Unexpected error communicating when trying to connect to Queenbee. "
 
-      begin
-        # response = execute_request(request_opts)
-        uri = URI(url)
-        request = Net::HTTP::Post.new(uri)
+      when NoMethodError
+        message = "Unexpected HTTP response code"
 
-        request["User-Agent"] = "Queenbee gem"
-        request["Authorization"] = "Token token=\"#{token}\""
-        request["Content-Type"] = "application/json"
-
-        request.body = params.to_json
-
-        response = Net::HTTP.start(uri.hostname, uri.port) {|http|
-          http.request(request)
-        }
-
-        # since http.request doesn't throw any exceptions
-        handle_api_error(response.code, response.body)
-
-        puts response.code
-      rescue SocketError => e
-          handle_connection_error(e)
-      rescue NoMethodError => e
-          handle_connection_error(e)
-      rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,
-          Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError => e
-          handle_connection_error(e)
-      end
-
-      [response, token]
+      else
+        message = "Unexpected error communicating with Queenbee. "
     end
 
-    def self.handle_connection_error(e)
-      case e
-        when SocketError
-          message = "Unexpected error communicating when trying to connect to Queenbee. "
+    raise APIConnectionError.new(message + "\n\n(Network error: #{e.message})")
+  end
 
-        when NoMethodError
-          message = "Unexpected HTTP response code"
-
-        else
-          message = "Unexpected error communicating with Queenbee. "
-      end
-
-      raise APIConnectionError.new(message + "\n\n(Network error: #{e.message})")
+  def self.handle_api_error(rcode, rbody)
+    begin
+      error_obj = JSON.parse(rbody)
+    rescue JSON::ParserError
+      raise general_api_error(rcode, rbody)
     end
 
-    def self.handle_api_error(rcode, rbody)
-      begin
-        error_obj = JSON.parse(rbody)
-      rescue JSON::ParserError
-        raise general_api_error(rcode, rbody)
-      end
-
-      case rcode
-        when 400, 404
-          raise invalid_request_error error, rcode, rbody, error_obj
-        when 401
-          raise authentication_error error, rcode, rbody, error_obj
-        else
-          # raise api_error error, rcode, rbody, error_obj
-      end
-
+    case rcode
+      when 400, 404
+        raise invalid_request_error error, rcode, rbody, error_obj
+      when 401
+        raise authentication_error error, rcode, rbody, error_obj
+      else
+        # raise api_error error, rcode, rbody, error_obj
     end
 
-    def self.invalid_request_error(error, rcode, rbody, error_obj)
-      InvalidRequestError.new(error[:message], error[:param], rcode,
-                              rbody, error_obj)
-    end
+  end
 
-    def self.authentication_error(error, rcode, rbody, error_obj)
-      AuthenticationError.new(error[:message], rcode, rbody, error_obj)
-    end
+  def self.invalid_request_error(error, rcode, rbody, error_obj)
+    InvalidRequestError.new(error[:message], error[:param], rcode,
+                            rbody, error_obj)
+  end
 
-    def self.api_error(error, rcode, rbody, error_obj)
-      APIError.new(error[:message], rcode, rbody, error_obj)
-    end
+  def self.authentication_error(error, rcode, rbody, error_obj)
+    AuthenticationError.new(error[:message], rcode, rbody, error_obj)
+  end
 
-    def self.general_api_error(rcode, rbody)
-      APIError.new("Invalid response object from API: #{rbody.inspect} " +
-                       "(HTTP response code was #{rcode})", rcode, rbody)
-    end
+  def self.api_error(error, rcode, rbody, error_obj)
+    APIError.new(error[:message], rcode, rbody, error_obj)
+  end
+
+  def self.general_api_error(rcode, rbody)
+    APIError.new("Invalid response object from API: #{rbody.inspect} " +
+                     "(HTTP response code was #{rcode})", rcode, rbody)
+  end
 end
